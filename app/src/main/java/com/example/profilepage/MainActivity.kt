e3a0055b868f0f4fd54b2c3dba3f78aad64196e1
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-12 20:37:07 -0300

    testando um post

--- app/src/main/java/com/example/profilepage/MainActivity.kt
+++ app/src/main/java/com/example/profilepage/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.profilepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.BufferedReader
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val url = URL("https://postman-echo.com/post")
+        val postData = "foo1=bar1&foo2=bar2"
+
+        val conn = url.openConnection()
+        conn.doOutput = true
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
+        conn.setRequestProperty("Content-Length", postData.length.toString())
+
+        DataOutputStream(conn.getOutputStream()).use { it.writeBytes(postData) }
+        BufferedReader(InputStreamReader(conn.getInputStream())).use { bf ->
+            var line: String?
+            while (bf.readLine().also { line = it } != null) {
+                println(line)
+            }
+        }
     }
 }
