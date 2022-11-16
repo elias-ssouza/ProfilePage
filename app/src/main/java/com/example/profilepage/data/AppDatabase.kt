0084418e2fc7ba1cfd5400527510fb36ae599cb8
elias-ssouza <elias.ssouza@hotmail.com>
2022-11-15 23:38:41 -0300

    criação appdatabase

---
+++ app/src/main/java/com/example/profilepage/data/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.profilepage.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ProfilePage::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun profileDao(): ProfilePageDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "profilepage_db"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+
+}
